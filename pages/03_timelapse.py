8ad4960684ae0cc4e20471c0fa33d2246d69771c
Qiusheng Wu <giswqs@gmail.com>
2024-07-25 13:17:04 -0400

    Add playa boundary

--- pages/03_timelapse.py
+++ pages/03_timelapse.py
@@ -11,14 +11,53 @@ class Map(geemap.Map):
         super().__init__(**kwargs)
         self.add_basemap("Esri.WorldImagery")
         self.add_gui("timelapse", basemap=None)
+        self.add_ee_data()
+
+    def add_ee_data(self):
+
+        url = "https://fwspublicservices.wim.usgs.gov/wetlandsmapservice/services/Wetlands/MapServer/WMSServer?"
+        self.add_wms_layer(
+            url,
+            layers="1",
+            format="image/png",
+            transparent=True,
+            opacity=0.7,
+            name="NWI Wetlands",
+        )
+
+        style = {
+            "color": "000000ff",
+            "width": 2,
+            "lineType": "solid",
+            "fillColor": "FF000000",
+        }
+        states = ee.FeatureCollection("TIGER/2018/States")
+        self.addLayer(states.style(**style), {}, "US States")
+
+        fc = ee.FeatureCollection("projects/ee-giswqs/assets/Playa/High_Plains")
+
+        style = {
+            "color": "ff0000ff",
+            "width": 2,
+            "lineType": "solid",
+            "fillColor": "FF000000",
+        }
+        self.add_layer(fc.style(**style), {}, "High Plains")
+
+        self.add_legend(
+            builtin_legend="NWI",
+            position="bottomleft",
+            add_header=False,
+            title="Wetland Type",
+        )
 
 
 @solara.component
 def Page():
     with solara.Column(style={"min-width": "500px"}):
         Map.element(
-            center=[20, -0],
-            zoom=2,
+            center=[37.6045093, -102.414550],
+            zoom=6,
             height="750px",
             zoom_ctrl=False,
             measure_ctrl=False,
--- pages/05_jrc.py
+++ pages/05_jrc.py
@@ -15,6 +15,16 @@ class Map(geemap.Map):
 
     def add_ee_data(self):
 
+        url = "https://fwspublicservices.wim.usgs.gov/wetlandsmapservice/services/Wetlands/MapServer/WMSServer?"
+        self.add_wms_layer(
+            url,
+            layers="1",
+            format="image/png",
+            transparent=True,
+            opacity=0.7,
+            name="NWI Wetlands",
+        )
+
         dataset = ee.Image("JRC/GSW1_4/GlobalSurfaceWater")
         image = dataset.select(["occurrence"])
         vis_params = {
@@ -25,6 +35,22 @@ class Map(geemap.Map):
         self.addLayer(image, vis_params, "Occurrence")
         self.add_colorbar(
             vis_params, label="Water occurrence (%)", layer_name="Occurrence"
+        )
+
+        fc = ee.FeatureCollection("projects/ee-giswqs/assets/Playa/High_Plains")
+        style = {
+            "color": "000000ff",
+            "width": 2,
+            "lineType": "solid",
+            "fillColor": "FF000000",
+        }
+        self.add_layer(fc.style(**style), {}, "Playa")
+
+        self.add_legend(
+            builtin_legend="NWI",
+            position="bottomleft",
+            add_header=False,
+            title="Wetland Type",
         )
 
     def add_buttons(self, position="topright", **kwargs):
@@ -133,8 +159,8 @@ class Map(geemap.Map):
 def Page():
     with solara.Column(style={"min-width": "500px"}):
         Map.element(
-            center=[20, -0],
-            zoom=2,
+            center=[37.6045093, -102.414550],
+            zoom=6,
             height="750px",
             zoom_ctrl=False,
             measure_ctrl=False,
