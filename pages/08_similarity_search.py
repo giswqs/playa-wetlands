0e32ca8acbd84fde3e00988a986885b87d12ca30
Qiusheng Wu <giswqs@gmail.com>
2025-08-28 14:32:37 -0400

    Add polygons

--- pages/08_similarity_search.py
+++ pages/08_similarity_search.py
@@ -16,6 +16,20 @@ def create_map():
     )
     google_hybrid = "https://mt1.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}"
     m.add_tile_layer(google_hybrid, name="Google Hybrid", attribution="Google")
+    study_area_url = "https://github.com/giswqs/playa-wetlands/releases/download/datasets/Playa_Study_Area_4326.geojson"
+    aquifer_url = "https://github.com/giswqs/playa-wetlands/releases/download/datasets/High_Plains_Aquifer_4326.geojson"
+    m.add_geojson(
+        study_area_url,
+        name="Study Area",
+        layer_type="line",
+        paint={"line-color": "red", "line-width": 2},
+    )
+    m.add_geojson(
+        aquifer_url,
+        name="High Plains Aquifer",
+        layer_type="line",
+        paint={"line-color": "blue", "line-width": 2},
+    )
     layer_name = "NWI Wetlands"
     m.add_nwi_basemap(name=layer_name, opacity=0.5)
     m.add_similarity_search(
